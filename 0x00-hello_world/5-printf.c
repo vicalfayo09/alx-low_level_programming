31ab83cecf8f997ed1c937f05ff12166719c7d0a
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-08 08:54:07 -0700

    Updated 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
 /**
  *main- Entry point
  *Description: prints "with proper grammar, but the outcome is a piece of art"
- *return 0 outcome:(success)
+ *Return: Always 0(success)
  */
-int main(void){
-printf("%s","with proper grammar, but the outcome is a piece of art");
+int main(void)
+{
+printf("%s","with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
