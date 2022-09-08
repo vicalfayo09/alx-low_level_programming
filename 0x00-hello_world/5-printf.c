60dbba0fa8d1fce908ac649d6ff1222bbed53466
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-08 08:47:40 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *Description: prints "with proper grammar, but the outcome is a piece of art"
+ *return 0 outcome:(success)
+ */
+int main(void){
+printf("%s","with proper grammar, but the outcome is a piece of art");
+return (0);
+}
