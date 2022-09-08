4e1e74f24aad993293dcedb5ec33cac5aad5224f
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-08 10:29:43 -0700

    Added changes to this file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main- print quote
  * Description: prints Doras quote
@@ -8,5 +9,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);	
+	return (1);
 }
