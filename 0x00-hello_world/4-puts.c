8f91ea7735c6cebc7be5d300dbbafbeadfea763d
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-08 07:53:16 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
 
 /**
  *main - Entry point
- * 
- *Description: prints "Programming is like building a multilingual puzzle"
- * 
+ *Description: prints "Programming is like building a multilingual puzzle" 
  *Return: Always 0 (Success)
  */
 int main(void)
