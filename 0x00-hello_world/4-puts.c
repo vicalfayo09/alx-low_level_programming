d86f041d18ca4ba8bdbb9be7fb41aea191ca79f0
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-08 07:58:52 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *main - Entry point
  *Description: prints "Programming is like building a multilingual puzzle" 
  *Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
