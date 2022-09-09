4d2da44a0a0598e2aa7c225ef6896281b59c061b
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-09 15:17:44 -0700

    added 4-alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters of alphabet
+ * Description : prints leteers of alphabet except e and q
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i!= 113)
+			putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
