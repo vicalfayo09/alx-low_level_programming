df2ae8bcaff23e5a4848080c93756ef7e4b023a3
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-09 14:40:31 -0700

    added 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters in alphabetical order
+ * Description: prints lowercase letters in alphabetical order
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int i = 97;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
