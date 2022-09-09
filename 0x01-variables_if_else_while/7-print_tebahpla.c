ee14c0f12ba7f057d397c6e87789d899c3af981f
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-09 15:37:26 -0700

    Added 7-tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase in reverse
+ * Description: prints lowercase in reverse
+ * Return : Always 0(success)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
