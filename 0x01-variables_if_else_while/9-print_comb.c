7c2d4681561cde7754b3af49d79be237bdabda4c
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-09 17:02:26 -0700

    Added 9-print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -prints all possible combinations of single-digit numbers
- * Description: prints all possible combinations of single-digit numbers
- * Return : Always 0(success)
+ * main - print single-digit numbers
+ * Description: print single-digit numbers with commas
+ * Return: Always 0(success)
  */
 int main(void)
 {
