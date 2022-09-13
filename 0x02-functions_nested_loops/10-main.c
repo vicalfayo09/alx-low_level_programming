c4d86c556e8934a2e9625268b37de7ae7591a391
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 11:45:19 -0700

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Check the code
+ * Description: adds two integers
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
