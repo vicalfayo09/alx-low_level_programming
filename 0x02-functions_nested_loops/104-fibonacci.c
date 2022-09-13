f9ae5a61e71d988c89acdc6b1c1ec278006d727b
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 14:38:17 -0700

    Updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,12 +9,13 @@ int fib(int n)
 {
 	if (n <= 1)
 		return n;
-	return fib(n -1 ) + fib(n -2);
+	return fib(n - 1) + fib(n - 2);
 }
 int main(void)
 {
 	int i = 98;
+
 	printf("%d", fib(i));
 	getchar();
-	return 0;
+	return (0);
 }
