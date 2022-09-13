ccb82cb9f750b42e9cec02b5108ad4852538c302
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 14:32:58 -0700

    Updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,16 @@
  * Description: prints the first 98 Fibonacci numbers
  * Return: Always 0(Success)
  */
+int fib(int n)
+{
+	if (n <= 1)
+		return n;
+	return fib(n -1 ) + fib(n -2);
+}
 int main(void)
 {
-
+	int i = 98;
+	printf("%d", fib(i));
+	getchar();
+	return 0;
 }
