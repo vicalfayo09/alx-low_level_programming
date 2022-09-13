e086eb4b6b16e3c56b50a49b08ae2e4c42f61407
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 03:31:51 -0700

    Added 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ * Description: prints _putchar
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	printf("%cputchar\n",95);
+	return (0);
+}
