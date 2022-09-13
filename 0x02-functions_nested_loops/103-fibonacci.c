abb0f3b20d94e42a9b6a8f1f55fc54015e5cb7b5
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 14:08:08 -0700

    Added 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * main - prints the sum of all even Fibonacci numbers below 4,000,000
- * Description: prints the sum of the even-valued terms below 4,000,000
+ * Description: prints the sum of all even Fibonacci numbers below 4,000,000
  * Return : Always 0(Success)
  */
 int main(void)
