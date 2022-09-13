26ae66ccfe0f285c87264d4f002c593554af32bf
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 09:00:29 -0700

    Added 2-alphabet x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * Main: prints alphabets
+ * Description: prints alphabets 10 times
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	void print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 int _putchar(char);
-char myFunc(void);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
