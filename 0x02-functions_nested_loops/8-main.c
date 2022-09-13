652b077c6f4bc1bc0a786d0c988a710f2387e77b
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 11:16:54 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * Main: check the code
+ * Description: prints every minute of the day of Jack Bauer
+ * Return : Always 0(success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
