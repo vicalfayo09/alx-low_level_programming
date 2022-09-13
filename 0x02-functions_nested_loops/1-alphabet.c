eae37254fab4f88e0ff1dc53e2563708300b175a
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-13 06:38:58 -0700

    Added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - prints lowercase letters
- * Description: print lowercase letters
+ * Description: prints alphabets
  * Return: Always 0(Success).
  */
 void print_alphabet(void)
