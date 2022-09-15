51df79b440c12735df5b35b4f0352496dbd8a9d7
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-15 09:30:59 -0700

    Added 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int num, count;
 
-	for (count = 0; count <= 9; count++)
+	for (count = 0; count < = 9; count++)
 	{
-		for (num = 0; num <=14; num++)
+		for (num = 0; num < = 14; num++)
 		{
 			if (num > 9)
 				_putchar((num / 10) + '0');
