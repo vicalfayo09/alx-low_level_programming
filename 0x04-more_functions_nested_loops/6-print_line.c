e312b09e8b688e0ad81bf53242d662f699df5dfb
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-15 08:01:46 -0700

    Added 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in terminal
- * Description:  draws a straight line in terminal
+ * @n: n is an integer
+ * Description: draws a straight line in terminal
  * Return: void
  */
 void print_line(int n)
 {
 	int len;
+
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
