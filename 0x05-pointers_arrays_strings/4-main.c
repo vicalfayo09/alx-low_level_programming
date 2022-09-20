76960557750310ed4d32fe652c7d8fa8da2ce8e8
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-20 14:36:42 -0700

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: Prints a string in reverse
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
