c35a79207d6d1020f08b576b28ce461f229a2256
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-20 15:21:12 -0700

    Added 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: Prints half of a string
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
