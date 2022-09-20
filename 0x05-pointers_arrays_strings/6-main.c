fb9d0a6eb119943cde8cbe8bfd32de65a9411690
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-20 15:09:36 -0700

    Added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: Prints one char out of two of a string
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
