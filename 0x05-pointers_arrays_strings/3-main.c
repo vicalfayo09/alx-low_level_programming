f325239eaabd2bece7639d4cc78bc91816688232
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-20 14:14:59 -0700

    Added 3-puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: Prints a string to stdout
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
