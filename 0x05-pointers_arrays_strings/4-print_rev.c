93d03047ee17459c491b6d5f8478eb040861eed3
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-20 19:43:53 -0700

    Added 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,19 @@
 /**
  * print_rev - Prints a string in reverse
  * @s: The string to be printed
+ * Return: void
  */
 void print_rev(char *s)
 {
 	int len = 0, index;
 
 	while (s[index++])
+	{
 		len++;
+	}
 	for (index = len - 1; index >= 0; index--)
+	{
 		_putchar(s[index]);
+	}
 	_putchar('\n');
 }
