93f09222fff825cbab9fabffdf081fa1bf3ed317
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-21 02:07:35 -0700

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Description: Changes all lowercase letters of a string to uppercase
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char str[] = "Look up!\n";
+	char *ptr;
+
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
