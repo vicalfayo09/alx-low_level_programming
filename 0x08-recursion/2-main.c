7a2bd25ff825031919c3b0fbfaf4c1576e5c74e4
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-29 12:35:25 -0700

    Added 2-strlen_recursion.c

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Description: returns length of string
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
