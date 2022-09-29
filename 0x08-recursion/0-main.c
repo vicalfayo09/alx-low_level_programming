66d012103209db70cc3ca19ec67dcfb56814ca5b
vicalfayo09 <victoralfayo09@gmail.com>
2022-09-29 11:31:50 -0700

    Added 0-puts_recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Description: prints a string, followed by a new line
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
