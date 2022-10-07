bbe05ba7853407fd527b7d21c67872f3f3d6bb2b
vicalfayo09 <80537304+vicalfayo09@users.noreply.github.com>
2022-10-08 00:17:27 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
@@ -33,7 +40,7 @@ char *create_xarray(int size)
 
 	array = malloc(sizeof(char) * size);
 
-	if (array == NULL)
+	if (!array)
 		exit(98);
 
 	for (index = 0; index < (size - 1); index++)
