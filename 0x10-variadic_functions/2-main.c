52bd85254077016cf6d13a00a0c0a42b2d52ce80
vicalfayo09 <victoralfayo09@gmail.com>
2022-10-15 10:49:09 -0700

    Added 3-pritnall.c

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * main - check the code
+ * Description: prints strings, followed by a new line
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
